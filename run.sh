70f5393cb50154446ef26f2357448e807129f722
Maximiliano Osorio <maxiosorio@gmail.com>
2025-04-16 14:18:14 -0400

    add: tutorial path

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ ls -ld ${ELMFIRE_BASE_DIR}
 
 cp -r ${ELMFIRE_BASE_DIR} ${_tapisExecSystemExecDir}
 ls -ld ${_tapisExecSystemExecDir}
-cd ${_tapisExecSystemExecDir}/elmfire
+cd ${_tapisExecSystemExecDir}/elmfire/tutorials/01-constant-wind
 ./01-run.sh
 
 ls
