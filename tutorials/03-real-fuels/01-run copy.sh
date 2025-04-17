c664e3733897e244350d69a190271cfc99125339
Will Mobley <feulmob+github@gmail.com>
2025-04-17 15:18:49 -0500

    Add copy of run and restored 01-run.sh to original tutorial

--- tutorials/03-real-fuels/01-run copy.sh
+++ tutorials/03-real-fuels/01-run copy.sh
@@ -2,10 +2,10 @@
 
 # Get fuel data for a tile:
 $ELMFIRE_BASE_DIR/cloudfire/fuel_wx_ign.py \
---do_wx=False --do_ignition=False \
---center_lon=-120.281 --center_lat=37.440 \
---fuel_source='landfire' --fuel_version='2.2.0' \
---outdir='./fuel' --name='tutorial03'
+    --do_wx=False --do_ignition=False \
+    --center_lon=-104.00304 --center_lat=30.633154 \
+    --fuel_source='landfire' --fuel_version='2.4.0' \
+    --outdir='./fuel' --name='tutorial03'
 
 SIMULATION_TSTOP=259200.0 # Simulation stop time (seconds)
 WX_INPUTS_FILE=wx.csv
