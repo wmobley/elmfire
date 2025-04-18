82a81a9d211b6db6357325570c1dcb8701433693
Will Mobley <feulmob+github@gmail.com>
2025-04-18 09:20:56 -0500

    Trying Tutorial 2

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ ls -ld ${ELMFIRE_BASE_DIR}
 
 cp -r ${ELMFIRE_BASE_DIR} ${_tapisExecSystemExecDir}
 ls -ld ${_tapisExecSystemExecDir}
-cd ${_tapisExecSystemExecDir}/elmfire/tutorials/01-constant-wind
+cd ${_tapisExecSystemExecDir}/elmfire/tutorials/02-transient-wind/
 ./01-run.sh
 
 ls
