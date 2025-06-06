631ce87f85226709ba892ba1be87ecc77207c5c5
Will Mobley <feulmob+github@gmail.com>
2025-06-06 11:43:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ env
 ls -ld ${ELMFIRE_BASE_DIR}
 
 echo "Present Variables"
-echo "1 $1, 2 $2, 3 $3, 4 $4"
+echo "1 $1, 2 $2, w.csv ${_tapisExecSystemInputDir}/wx.csv,  3 $3"
 find -L $(echo $PATH | tr ":" " ") -name 'elmfire' -type f -perm -001 -print 
 cp -r ${ELMFIRE_BASE_DIR} ${_tapisExecSystemExecDir}
 # ls -ld ${_tapisExecSystemExecDir}
 cd ${_tapisExecSystemExecDir}/elmfire/tutorials/03-real-fuels/
 # cd ${ELMFIRE_BASE_DIR}/tutorials/03-real-fuels/
-./01-run.sh $1 $2 $3 $4 
+./01-run.sh $1 $2 "${_tapisExecSystemInputDir}/wx.csv" $3 
 
 ls
 cp -r outputs ${_tapisExecSystemOutputDir}
